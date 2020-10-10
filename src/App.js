e9e9416a6f89ab0552c2ff7962bf9b84a70695b7
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-10-10 16:50:59 -0700

    image renders

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Dropdown from "react-dropdown";
-import parse from "html-react-parser";
-
-import { formatSeasons } from "./utils/formatSeasons";
-
-import Episodes from "./components/Episodes";
-import "./styles.css";
+import React, { useState, useEffect } from 'react'
+import Dropdown from 'react-dropdown'
+import Select from 'react-dropdown-select'
+import parse from 'html-react-parser'
+import { formatSeasons } from './utils/formatSeasons'
+import {fetchShow} from './api/fetchShow'
+import Episodes from './components/Episodes'
+import './styles.css'
 
 export default function App() {
   const [show, setShow] = useState(null);
   const [seasons, setSeasons] = useState([]);
   const [selectedSeason, setSelectedSeason] = useState("");
   const episodes = seasons[selectedSeason] || [];
+  const [options] = useState([]);
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    fetchShow()
+    .then(res => {
+      console.log('Response from App', res)
+      setShow(res.data)
+      setSeasons(formatSeasons(res.data._embedded.episodes))})
+    .catch(err => {
+      console.log(err)
+    })
   }, []);
 
+
+  
   const handleSelect = e => {
     setSelectedSeason(e.value);
   };
@@ -35,19 +34,16 @@ export default function App() {
   if (!show) {
     return <h2>Fetching data...</h2>;
   }
-
-  return (
-    <div className="App">
-      <img className="poster-img" src={show.image.original} alt={show.name} />
-      <h1>{show.name}</h1>
-      {parse(show.summary)}
-      <Dropdown
-        options={Object.keys(seasons)}
-        onChange={handleSelect}
-        value={selectedSeason || "Select a season"}
-        placeholder="Select an option"
-      />
-      <Episodes episodes={episodes} />
-    </div>
+    return (
+      <div data-testid='container'className="App">
+        <img className="poster-img" src={show.image.original} alt={show.name} />
+        <h1>{show.name}</h1>
+        {parse(show.summary)}
+        <Episodes episodes={episodes} />
+        <Select options={options} onChange={(values) => this.setValues(values)} />S
+      </div>
+    
   );
+
+
 }
